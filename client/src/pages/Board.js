4e09d68d57e6d02cc362935660eefdf49df8fe27
schnur.ariel@gmail.com <schnur.ariel@gmail.com>
2021-03-01 18:08:53 -0500

    Sort items by status

--- client/src/pages/Board.js
+++ client/src/pages/Board.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { Container, Columns } from "react-bulma-components";
@@ -14,7 +15,7 @@ const Board = (props) => {
             const { data } = await axios.get("/api/user/board_items", {
                 params: { name }
             });
-            setItems(data.items);
+            setItems(getItemsByStatus(data.items));
             setIsLoading(false);
         }
         fetchItems();
@@ -26,14 +27,46 @@ const Board = (props) => {
 
     return (
         <Container>
+            <h1>{name}</h1>
             <Columns>
-                <h1>{name}</h1>
-                {items.map((item) => {
-                    return <Item item={item} key={item.name} />;
-                })}
+                <Columns.Column>
+                    <h2>To Do</h2>
+                    {items.todo.map((item) => {
+                        return <Item item={item} key={item.name} />;
+                    })}
+                </Columns.Column>
+                <Columns.Column>
+                    <h2>In Progress</h2>
+                    {items.inprogress.map((item) => {
+                        return <Item item={item} key={item.name} />;
+                    })}
+                </Columns.Column>
+                <Columns.Column>
+                    <h2>Done</h2>
+                    {items.done.map((item) => {
+                        return <Item item={item} key={item.name} />;
+                    })}
+                </Columns.Column>
             </Columns>
         </Container>
     );
+};
+
+/**
+ * Order items first by status and then by priority
+ */
+const getItemsByStatus = (items) => {
+    let itemsInOrder = {
+        todo: sortItems(_.filter(items, { status: "todo" })),
+        inprogress: sortItems(_.filter(items, { status: "inprogress" })),
+        done: sortItems(_.filter(items, { status: "done" }))
+    };
+
+    return itemsInOrder;
+};
+
+const sortItems = (items) => {
+    return _.sortBy(items, "priority");
 };
 
 export default Board;
