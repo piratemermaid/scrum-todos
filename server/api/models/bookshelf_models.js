8577a8a7f30dc35e643c3427e85306cb30d52c10
schnur.ariel@gmail.com <schnur.ariel@gmail.com>
2021-03-01 16:58:05 -0500

    Set up bookshelf models and API for user data

--- server/api/models/bookshelf_models.js
+++ server/api/models/bookshelf_models.js
@@ -1,7 +1,28 @@
 const { bookshelf } = require("./config");
 
 const User = bookshelf.model("User", {
-    tableName: "users"
+    tableName: "users",
+    boards() {
+        return this.belongsToMany("Board", "users_boards");
+    }
+});
+
+const Board = bookshelf.model("Board", {
+    tableName: "boards",
+    items() {
+        return this.belongsToMany("Item", "boards_items");
+    }
+});
+
+const Item = bookshelf.model("Item", {
+    tableName: "items",
+    tags() {
+        return this.belongsToMany("Tag", "items_tags");
+    }
+});
+
+const Tag = bookshelf.model("Tag", {
+    tableName: "tags"
 });
 
 module.exports = { User };
--- server/api/routes/user.js
+++ server/api/routes/user.js
@@ -1,9 +1,57 @@
 const { Router } = require("express");
+const models = require("../models/bookshelf_models");
+const { Session } = require("../helpers/account");
 
 const router = new Router();
 
-router.get("/", (req, res) => {
-    res.send({ api: "user" });
+router.get("/", async (req, res) => {
+    console.log("route hit");
+    const { sessionString } = req.cookies;
+
+    if (!sessionString || !Session.verify(sessionString)) {
+        const error = new Error("Invalid session");
+
+        error.status = 400;
+
+        return next(error);
+    } else {
+        const { username } = Session.parse(sessionString);
+
+        const userData = await new models.User({ username }).fetch({
+            withRelated: ["boards.items.tags"]
+        });
+
+        res.send({
+            boards: userData.toJSON().boards.map(({ name, items }) => {
+                return {
+                    name,
+                    items: items.map(
+                        ({
+                            name,
+                            priority,
+                            notes,
+                            status,
+                            blocker,
+                            repeat,
+                            tags
+                        }) => {
+                            return {
+                                name,
+                                priority,
+                                notes,
+                                status,
+                                blocker,
+                                repeat,
+                                tags: tags.map(({ name }) => {
+                                    return name;
+                                })
+                            };
+                        }
+                    )
+                };
+            })
+        });
+    }
 });
 
 module.exports = router;
