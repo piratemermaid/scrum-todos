a7b9cef47cb2496be0854b31cf1a0d5a8f835c1a
schnur.ariel@gmail.com <schnur.ariel@gmail.com>
2021-03-01 17:03:16 -0500

    Get data in UI

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { withRouter } from "react-router";
 
 const Home = (props) => {
+    const [boards, setBoards] = useState([]);
+
+    useEffect(() => {
+        async function fetchUserData() {
+            try {
+                const { data } = await axios.get("/api/user");
+                setBoards(data.boards);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        fetchUserData();
+    }, []);
+
     const logOut = () => {
         axios({
             method: "get",
@@ -23,14 +37,38 @@ const Home = (props) => {
     return (
         <div>
             <h1>Home</h1>
-            {!props.authenticated ? (
-                <div>
-                    <Link to="/login">Log In</Link> |{" "}
-                    <Link to="/signup">Sign Up</Link>
-                </div>
-            ) : (
-                <a onClick={logOut}>Log Out</a>
-            )}
+            <h2>Your Boards</h2>
+            {boards.map(({ name, items }) => {
+                return (
+                    <div>
+                        <h3>{name}</h3>
+                        <ul>
+                            {items.map(
+                                ({
+                                    name,
+                                    priority,
+                                    notes,
+                                    status,
+                                    blocker,
+                                    repeat,
+                                    tags
+                                }) => {
+                                    return (
+                                        <li>
+                                            {name}
+                                            {tags.length > 0 && (
+                                                <span>
+                                                    {` (${tags.join(", ")})`}
+                                                </span>
+                                            )}
+                                        </li>
+                                    );
+                                }
+                            )}
+                        </ul>
+                    </div>
+                );
+            })}
         </div>
     );
 };
