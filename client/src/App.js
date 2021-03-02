8b1828801603d55ec4c8a995407986d3c6fe7b47
schnur.ariel@gmail.com <schnur.ariel@gmail.com>
2021-03-02 18:12:31 -0500

    Remove unecessary create-react-app files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
-import "./App.scss";
 import "./styles/main.scss";
 import "react-bulma-components/dist/react-bulma-components.min.css";
 
