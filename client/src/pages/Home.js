b2b13b7c630139ce6d8cb2d1d687bb44b93b73f8
schnur.ariel@gmail.com <schnur.ariel@gmail.com>
2021-03-13 10:18:30 -0500

    Add some styling to board list

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useContext } from "react";
 import { withRouter, useHistory } from "react-router";
 import BoardsContext from "../context/BoardsContext";
+import { Columns, Card } from "react-bulma-components";
+import "../styles/home.scss";
 
 const Home = (props) => {
     const [isLoading, setIsLoading] = useState(true);
@@ -20,19 +22,29 @@ const Home = (props) => {
     }
 
     return (
-        <div>
+        <div id="home">
             <h1>Home</h1>
             <h2 className="is-size-3">Your Boards</h2>
-            {boards.map(({ name }) => {
+            {boards.map(({ name, items }) => {
                 return (
-                    <div
-                        onClick={() => {
-                            history.push(`/board/${name}`);
-                        }}
-                        key={name}
-                    >
-                        <h3 className="board-name is-size-4">{name}</h3>
-                    </div>
+                    <Columns className="is-centered board-list">
+                        <Columns.Column className="has-text-centered is-one-third">
+                            <Card
+                                className="board-card"
+                                onClick={() => {
+                                    history.push(`/board/${name}`);
+                                }}
+                                key={name}
+                            >
+                                <Card.Content>
+                                    <h3 className="board-name is-size-4">
+                                        {name}
+                                    </h3>
+                                    <p>{items.length} items</p>
+                                </Card.Content>
+                            </Card>
+                        </Columns.Column>
+                    </Columns>
                 );
             })}
         </div>
